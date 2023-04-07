4e42348a0f83533b28f6ebd44df0b73723bfe6c8
ShayanHodai <shayan.hodai@outlook.com>
2023-04-06 19:42:39 -0500

    environment & packages

--- env.sh
+++ env.sh
@@ -10,4 +10,3 @@ pip install matplotlib
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 echo 
 echo 'Done! as of 2023-04-06 lastest required packages are installed succesfully.'
-
