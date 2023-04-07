3cca83ae4a0cde1cf224b9b09f1290257cba1ee6
ShayanHodai <shayan.hodai@outlook.com>
2023-04-06 19:42:14 -0500

    env.sh

--- env.sh
+++ env.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 # latest packages as of 2023-04-06
 
-python3 -m venv $(pwd)/venv/twitter
-source $(pwd)/venv/twitter/bin/activate
+python3 -m venv $(pwd)/venv/protein
+source $(pwd)/venv/protein/bin/activate
 pip install --upgrade pip
 pip install pandas
 pip install numpy
 pip install matplotlib
-pip install transformers
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 echo 
 echo 'Done! as of 2023-04-06 lastest required packages are installed succesfully.'
